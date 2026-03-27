c46e5e9d214a4c3797664c5d85f82e623e1ca719
Hamna-Sajid <hamna.sajid88@gmail.com>
2026-03-27 23:28:10 +0500

    fix(master): reduce number of epochs from 20 to 10 for training

--- run_master.py
+++ run_master.py
@@ -2,5 +2,5 @@
 from coordination.master import Master
 
 if __name__ == "__main__":
-    master = Master(n_workers=2, n_epochs=20, lr=0.01)
+    master = Master(n_workers=2, n_epochs=10, lr=0.01)
     master.run()
