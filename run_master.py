20548b04bc9b034da1da0912b42cf471b8646972
Hamna-Sajid <hamna.sajid88@gmail.com>
2026-04-21 01:18:55 +0500

    Revert "Merge milestone/m2-heterogeneity-handling into main"
    This reverts commit 7e3f55b7912d0c09bd05e430045681733de47a0d, reversing
    changes made to f665bf94d4d83394cb03c04fbcc8dacdff2c29e4.

--- run_master.py
+++
@@ -1,6 +0,0 @@
-"""Entry point to start the master node."""
-from coordination.master import Master
-
-if __name__ == "__main__":
-    master = Master(n_workers=2, n_epochs=10, lr=0.01)
-    master.run()
--- setup.py
+++
@@ -1,7 +0,0 @@
-# setup.py
-from setuptools import setup, find_packages
-
-setup(
-    name="distributed-nn-training",
-    packages=find_packages(),
-)
